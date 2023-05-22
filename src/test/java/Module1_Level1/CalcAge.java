0e6daf18a2194ba1f638fa3e89d7762935bcf9c8
flavio_morassuti <flavio_morassuti@epam.com>
2023-05-22 09:55:30 +0200

    Project skeleton added

---
+++ src/test/java/Module1_Level1/CalcAge.java
@@ -0,0 +1,22 @@
+package Module1_Level1;
+
+public class CalcAge {
+
+    public static void main(String[] args){
+        Person person = new Person();
+        person.name = "Tom";
+        person.age = 18;
+        person.address = "Holzstrasse";
+        System.out.println(person.name);
+        person.adultPerson();
+
+        Person person2 = new Person();
+        person2.name = "Julie";
+        person2.age = 15;
+        person2.address = "Sewastrasse";
+        System.out.println(person2.name);
+        person2.adultPerson();
+
+
+    }
+}
---
+++ src/test/java/Module1_Level1/Pairs.java
@@ -0,0 +1,12 @@
+package Module1_Level1;
+
+public class Pairs {
+    public static void main (String[] args){
+        int i;
+        for (i = 1; i<=100; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
