dc4c5a15f34b4089b11da5d15e9e8898f8c5b3e9
flavio_morassuti <flavio_morassuti@epam.com>
2023-06-08 07:47:43 +0200

    Project skeleton added

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Calculator {
+    public static int add (int a, int b){
+        return a + b;
+    }
+
+    public static int expo(int base, int exponent){
+        return (int) Math.pow(base, exponent);
+
+    }
+}
